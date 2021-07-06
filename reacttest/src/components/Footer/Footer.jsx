9dd2ce8c0ccbb2beb73e8bc997cdba808b06e8c1
Vincenzo <vincenzo.cavataio@nextre.it>
2021-07-06 18:02:49 +0200

    introduced Styled Components

--- reacttest/src/components/Footer/Footer.jsx
+++ reacttest/src/components/Footer/Footer.jsx
@@ -1,13 +1,14 @@
 import React, { Component } from 'react'
 import NavMenu from '../NavMenu'
+import FooterS from './styled'
 import './style.css'
 
 export class Footer extends Component {
     render() {
         return (
-            <footer>
+            <FooterS>
                 <NavMenu logo={this.props.logo} imgName={this.props.imgName} links={this.props.links}/>
-            </footer>
+            </FooterS>
         )
     }
 }
