909886822da26bd2601770b013c86cd4c923608c
Gabriele Corti <gabriele.corti.lavoro@gmail.com>
2021-07-06 12:13:00 +0200

    add: Carousel

--- reacttest/src/components/carousel/Carousel.jsx
+++ reacttest/src/components/carousel/Carousel.jsx
@@ -1,11 +1,28 @@
 import React, { Component } from 'react'
-import './style.css'
+import './style.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronRight, faChevronLeft } from '@fortawesome/free-solid-svg-icons';
 
 export class Carousel extends Component {
     render() {
         return (
-            <div>
-                
+            <div className='Carousel'>
+                <div className="slideShow">
+                    <div>
+                        <FontAwesomeIcon className='arrow' icon={faChevronLeft} />
+                    </div>
+                    <div className="slide">
+                        <img src={this.props.img} alt="" />
+                        <div className="img_info">
+                            <h3 className='slide_title'>{this.props.title}</h3>
+                            <p className='slide_description'>{this.props.description}</p>
+                            <a>{this.props.link}</a>
+                        </div>
+                    </div>
+                    <div>
+                        <FontAwesomeIcon className='arrow' icon={faChevronRight} />    
+                    </div>
+                </div>
             </div>
         )
     }
--- reacttest/src/index.jsx
+++ reacttest/src/index.jsx
@@ -7,6 +7,7 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Jumbotron from "./components/Jumbotron";
 import InfoCard from "./components/InfoCard";
+import Carousel from "./components/Carousel";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -21,9 +22,17 @@ ReactDOM.render(
         title="Viaggi e sogni"
         clame="sogna viaggiando"
         action="viaggia qui"
-        btnClass='btn white big'
+        btnClass="btn white big"
       />
-      <InfoCard /*ci va la props info, per pulizia ora la metto in state* info={array}*/ cardClass='' />
+      <InfoCard
+        /*ci va la props info, per pulizia ora la metto in state* info={array}*/ cardClass=""
+      />
+      <Carousel
+        img="https://i.pinimg.com/originals/c2/05/4c/c2054cd58c7e0f42744bdb84b17faae4.jpg"
+        title="Foresta Nera"
+        description="Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa."
+        links="articoli riguardo al Foresta Nera"
+      />
     </App>
     <Footer
       logo="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTABSta4ztO2Z73YCEvZDFgCPesndhqt-seBg&usqp=CAU"
