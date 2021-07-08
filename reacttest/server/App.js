9f0871aa62051b83782d5c6317804ecbecbfcee3
Gabriele Corti <gabriele.corti.lavoro@gmail.com>
2021-07-08 12:06:37 +0200

    add: funzione filtro suggeriti

--- reacttest/server/App.js
+++ reacttest/server/App.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const moment = require('moment')
-var dayjs = require('dayjs')
+const moment = require("moment");
+var dayjs = require("dayjs");
+var isTomorrow = require("dayjs/plugin/isTomorrow");
+var isYesterday = require('dayjs/plugin/isYesterday');
 const port = 8000;
+
+//plugin di dayJs
+dayjs.extend(isTomorrow);
+dayjs.extend(isYesterday);
 
 //serve per dare i permessi CORS a tutte le rotte
 app.use(cors());
@@ -15,8 +21,8 @@ app.use(express.json());
 const Data = [
   {
     id: 1,
-    andata: "12-1-2021",
-    ritorno: "12-1-2021",
+    andata: "2021-12-02",
+    ritorno: "2021-12-02",
     da: "Milano",
     a: "Roma",
     mezzo: "Aereo",
@@ -25,46 +31,63 @@ const Data = [
   },
   {
     id: 2,
-    andata: "4-1-2021",
-    ritorno: "9-6-2021",
-    da: "Torino",
-    a: "Venezia",
+    andata: "2021-12-01",
+    ritorno: "2021-12-02",
+    da: "Milano",
+    a: "Roma",
     mezzo: "Aereo",
     prezzo: 400,
   },
   {
     id: 3,
-    andata: "12-4-2021",
-    ritorno: "12-5-2021",
-    da: "Napoli",
-    a: "Aosta",
+    andata: "2021-12-02",
+    ritorno: "2021-12-03",
+    da: "Milano",
+    a: "Roma",
     mezzo: "Aereo",
     prezzo: 600,
   },
   {
     id: 4,
-    andata: "12-1-2021",
-    ritorno: "4-2-2021",
+    andata: "12-01-2021",
+    ritorno: "4-02-2021",
     da: "Milano",
     a: "Roma",
     mezzo: "Aereo",
     prezzo: 600,
-  }
+  },
 ];
 
 app.get("/", (req, res) => {
   const UserReq = req.query;
   /* console.log(UserReq); */
-  
-  const Price = Data.filter(element => {
-      return element.andata == UserReq.andata &&
-             element.ritorno == UserReq.ritorno &&
-             element.da == UserReq.da &&
-             element.a == UserReq.a
-  })
+
+  const Price = Data.filter((element) => {
+    return (
+      dayjs(element.andata).format('DD/MM/YYYY') == dayjs(UserReq.andata).format('DD/MM/YYYY') &&
+      dayjs(element.ritorno).format('DD/MM/YYYY') == dayjs(UserReq.ritorno).format('DD/MM/YYYY') &&
+      element.da == UserReq.da &&
+      element.a == UserReq.a
+    );
+  });
+
+  const Suggested = Data.filter((element) => {
+    let dayBefore = dayjs(UserReq.andata).add(-1, 'day').format("DD/MM/YYYY") == dayjs(element.andata).add(-1, 'day').format("DD/MM/YYYY");
+    let dayAfter = dayjs(UserReq.ritorno).add(1, 'day').format("DD/MM/YYYY")== dayjs(element.ritorno).add(1, 'day').format("DD/MM/YYYY");
+    let isValid = false;
+
+    if (dayBefore || dayAfter) {
+        isValid = true;
+    }
+
+    return (
+      element.da == UserReq.da &&
+      element.a == UserReq.a && isValid
+    );
+  });
   /* console.log(moment(UserReq.andata, 'YYYY-DD-DD')); */
-  console.log(dayjs(UserReq.andata).format('DD/MM/YYYY'));
-  res.json(Price);
+  /* console.log(dayjs(UserReq.andata).format('DD/MM/YYYY')); */
+  res.json(Suggested);
   /* res.send(); */
 });
 
